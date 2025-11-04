0071d8538f6c70734cede78bfcbba157c9c6c392
Dikshit Raj Sharma <214721236+Dikshit-Raj-Sharma@users.noreply.github.com>
2025-11-04 13:05:44 +0000

    Change console output to UI output

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
+const apiKey = "646486bcc4955c3ae6c1030129fee1f2";
 const searchBtn = document.getElementById("search");
 const placeInput = document.getElementById("place");
-const apiKey = "646486bcc4955c3ae6c1030129fee1f2";
+const weatherIcon = document.getElementById("weather-icon");
+const cityName = document.getElementById("city-name");
+const condition = document.getElementById("condition");
+const err = document.getElementById("error-message");
+const temperature = document.getElementById("temperature");
+const weatherResult = document.getElementById("weather-results");
 
 searchBtn.addEventListener('click', () => {
     const inputText = placeInput.value.toLowerCase();
     fetchData(inputText);
 });
 async function fetchData(location) {
+    cityName.textContent="";
+    weatherIcon.src="";
+    condition.textContent="";
+    err.textContent="";
+    temperature.textContent="";
     try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`);
 
         if (!response.ok) {
-            throw new Error("An error occurred :(");
+            throw new Error("City Not Found. Please Try again.");
         }
         const data = await response.json();
-        console.log("City:", data.name);
-        console.log("Temperature:", data.main.temp);
-        console.log("Condition:", data.weather[0].description);
+        cityName.textContent=data.name;
+        temperature.textContent=`${Math.round(data.main.temp)}°C`;
+        condition.textContent=data.weather[0].description;
+        const iconCode = data.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+        weatherIcon.src=iconUrl;
+        weatherIcon.alt=data.weather[0].description;
+        weatherResult.classList.add("active");
     }
 
     catch (error) {
-        console.error(error.message);
+        err.textContent=error.message;
+        weatherResult.classList.add("active");
     }
 }
